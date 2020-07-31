0f0ec84e3d41f295c927f83116fc127e6c6a7eed
lucasmrpinho <lucaspinho101@gmail.com>
2020-07-31 18:11:17 -0300

    iniciando paginacao

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,34 @@ for (item of menuItens){
         item.classList.add('active') 
     }
 }
+
+
+function pagination(totalPages, selectedPage){
+    let pages = [],
+    oldPage
+
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if ( firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage ){
+            if(oldPage && currentPage - oldPage > 2){
+                pages.push("...")
+            }
+
+            if(oldPage && currentPage - oldPage == 2){
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
+        }
+
+    }
+
+    return pages
+
+}
